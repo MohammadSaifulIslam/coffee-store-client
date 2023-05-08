6b15b633c5d541c7b18382454a72d9e8bd90bb2d
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-05-08 15:33:35 +0600

    coffee item get and send it to server side fuction added

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,18 +1,59 @@
+import Swal from 'sweetalert2'
 const AddCoffee = () => {
+    const handleAddCoffee = event =>{
+        event.preventDefault();
+        const form = event.target;
+
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+        const newCoffee = {
+            name,
+            chef,
+            supplier,
+            taste,
+            category,
+            details,
+            photo
+        }
+        console.log(newCoffee)
+
+        fetch('http://localhost:5000/coffee',{
+            method : "POST",
+            headers: {
+                'content-type' : "application/json"
+            },
+            body: JSON.stringify(newCoffee)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data)
+            Swal.fire({
+                title: 'Successfully!',
+                text: 'Coffee insert to database',
+                icon: 'success',
+                confirmButtonText: 'ok'
+              })
+        })
+    }
     return (
         <div className="my-container bg-[#F4F3F0] py-16 px-28 mt-12">
             <div className="section-title">
                 <h1>Add New Coffee</h1>
                 <p>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
             </div>
-            <form >
+            <form onSubmit={handleAddCoffee}>
                 <div className="flex gap-5 mt-6">
                     <div className="form-control w-full">
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
                         <label className="">
-                            <input type="text" placeholder="Enter coffee name" className="input input-bordered w-full" />
+                            <input type="text" name="name" placeholder="Enter coffee name" className="input input-bordered w-full" />
                         </label>
                     </div>
                     <div className="form-control w-full">
@@ -20,7 +61,7 @@ const AddCoffee = () => {
                             <span className="label-text">Chef</span>
                         </label>
                         <label className="">
-                            <input type="text" placeholder="Enter chef name" className="input input-bordered w-full" />
+                            <input type="text" name="chef" placeholder="Enter chef name" className="input input-bordered w-full" />
                         </label>
                     </div>
                 </div>
@@ -30,7 +71,7 @@ const AddCoffee = () => {
                             <span className="label-text">Supplier</span>
                         </label>
                         <label className="">
-                            <input type="text" placeholder="Enter coffee supplier" className="input input-bordered w-full" />
+                            <input type="text" name="supplier" placeholder="Enter coffee supplier" className="input input-bordered w-full" />
                         </label>
                     </div>
                     <div className="form-control w-full">
@@ -38,7 +79,7 @@ const AddCoffee = () => {
                             <span className="label-text">Taste</span>
                         </label>
                         <label className="">
-                            <input type="text" placeholder="Enter coffee taste" className="input input-bordered w-full" />
+                            <input type="text" name="taste" placeholder="Enter coffee taste" className="input input-bordered w-full" />
                         </label>
                     </div>
                 </div>
@@ -48,7 +89,7 @@ const AddCoffee = () => {
                             <span className="label-text">Category</span>
                         </label>
                         <label className="">
-                            <input type="text" placeholder="Enter coffee category" className="input input-bordered w-full" />
+                            <input type="text" name="category" placeholder="Enter coffee category" className="input input-bordered w-full" />
                         </label>
                     </div>
                     <div className="form-control w-full">
@@ -56,7 +97,7 @@ const AddCoffee = () => {
                             <span className="label-text">Details</span>
                         </label>
                         <label className="">
-                            <input type="text" placeholder="Enter coffee details" className="input input-bordered w-full" />
+                            <input type="text" name="details" placeholder="Enter coffee details" className="input input-bordered w-full" />
                         </label>
                     </div>
                 </div>
@@ -66,7 +107,7 @@ const AddCoffee = () => {
                             <span className="label-text">Photo</span>
                         </label>
                         <label className="">
-                            <input type="text" placeholder="Enter photo URL" className="input input-bordered w-full" />
+                            <input type="text" name="photo" placeholder="Enter photo URL" className="input input-bordered w-full" />
                         </label>
                     </div>
                 </div>
